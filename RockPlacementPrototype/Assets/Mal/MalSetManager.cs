77965154e56104ee2e42904632d7594c74dbf15a
hazlett <hazlett.206@gmail.com>
2015-05-26 17:08:51 -0400

    Test Video

--- RockPlacementPrototype/Assets/Mal/MalSetManager.cs
+++ RockPlacementPrototype/Assets/Mal/MalSetManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using OhioState.CanyonAdventure;
+using System;
 
 public class MalSetManager : MonoBehaviour {
 	private MalSet malSet;
@@ -8,14 +9,18 @@ public class MalSetManager : MonoBehaviour {
 	public Mal currentMal;
 	public int currentPromptID;
 	public Prompt currentPrompt;
-
+	public MovieTexture movie;
 	private bool responseMode;
 	private string responseText;
 	private float responseTimer, responseTime;
 	private int videoID;
+	private bool videoStarted;
 
 	void Start () {
+		MalSetLogger.Instance.Open ();
+		MalSetLogger.Instance.CreateEntry ("-- New Session Started --");
 		responseMode = false;
+		videoStarted = false;
 		responseTimer = 0.0f;
 		responseTime = 3.0f;
 		serializer = new MalSetSerializer ();
@@ -42,11 +47,13 @@ public class MalSetManager : MonoBehaviour {
 		if (malSet.AllAsked()) {
 			Debug.Log("All asked");
 		}
+
 		currentPromptID = 0;
 		videoID = -1;
 
 		currentMal = malSet.GetMal ();
 		currentMal.Asked = true;
+		MalSetLogger.Instance.CreateEntry ("");
 		Debug.Log("Current Mal: " + currentMal.id + " : " + currentMal.Asked);
 
 	}
@@ -62,11 +69,23 @@ public class MalSetManager : MonoBehaviour {
 			SetMal();
 			return;
 		}
+		if (movie != null) {
+			if (movie.isPlaying)
+			{
+				GUILayout.Label("MOVIE IS PLAYING");
+				return;
+			}
+		}
 		currentPrompt = currentMal.GetPrompt (currentPromptID);
 		GUILayout.Label(currentPrompt.Question);
 		if (currentPrompt.HasVideo()) {
 			GUILayout.Label("HAS VIDEO");
 			GUILayout.Label(currentPrompt.GetVideo(videoID).Filename);
+			if (!videoStarted)
+			{
+				videoStarted = true;
+				StartCoroutine(LoadVideo(currentPrompt.GetVideo(videoID).Filename));
+			}
 		}
 		if (currentPrompt.buttonComponents.Count == 0) {
 			responseMode = true;
@@ -82,9 +101,18 @@ public class MalSetManager : MonoBehaviour {
 					}
 					currentPromptID = bc.FollowUp;
 					videoID = bc.VideoFollowUpId;
-				
+					videoStarted = false;
+					MalSetLogger.Instance.CreateEntry(String.Format("{0}: {1}", currentPrompt.Question, bc.Text));
 				}
 			}
 		}
+	}
+	private IEnumerator LoadVideo(string fileName)
+	{
+		WWW www = new WWW ("file:///TurnOnLight1.ogv");
+		yield return www;
+		movie = www.movie;
+		movie.Play ();
+		Debug.Log ("Movie Play");
 	}
 }
