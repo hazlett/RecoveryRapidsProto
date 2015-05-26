56b9f1e40c964167765294b4f8f8ab40b06b2461
hazlett <hazlett.206@gmail.com>
2015-05-26 16:37:07 -0400

    First Commit

---
+++ RockPlacementPrototype/Assets/Mal/MalSetManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using OhioState.CanyonAdventure;
+
+public class MalSetManager : MonoBehaviour {
+	private MalSet malSet;
+	private MalSetSerializer serializer;
+	public Mal currentMal;
+	public int currentPromptID;
+	public Prompt currentPrompt;
+
+	private bool responseMode;
+	private string responseText;
+	private float responseTimer, responseTime;
+	private int videoID;
+
+	void Start () {
+		responseMode = false;
+		responseTimer = 0.0f;
+		responseTime = 3.0f;
+		serializer = new MalSetSerializer ();
+		malSet = serializer.Read ("MalQuestions.xml");
+		foreach (Mal mal in malSet.malList) {
+			Debug.Log(mal.id);
+		}
+		SetMal ();
+	}
+
+	void Update ()
+	{
+		if (responseMode) {
+			responseTimer += Time.deltaTime;
+		}
+		if (responseTimer > responseTime) {
+			responseMode = false;
+			responseTimer = 0;
+		}
+
+	}
+	private void SetMal()
+	{
+		if (malSet.AllAsked()) {
+			Debug.Log("All asked");
+		}
+		currentPromptID = 0;
+		videoID = -1;
+
+		currentMal = malSet.GetMal ();
+		currentMal.Asked = true;
+		Debug.Log("Current Mal: " + currentMal.id + " : " + currentMal.Asked);
+
+	}
+
+	void OnGUI()
+	{
+		if (responseMode) {
+			GUILayout.Label(responseText);
+			return;
+		}
+		if (currentPromptID == -1) {
+			currentMal.Asked = true;
+			SetMal();
+			return;
+		}
+		currentPrompt = currentMal.GetPrompt (currentPromptID);
+		GUILayout.Label(currentPrompt.Question);
+		if (currentPrompt.HasVideo()) {
+			GUILayout.Label("HAS VIDEO");
+			GUILayout.Label(currentPrompt.GetVideo(videoID).Filename);
+		}
+		if (currentPrompt.buttonComponents.Count == 0) {
+			responseMode = true;
+			responseText = currentPrompt.Question;
+			SetMal();
+		} 
+		else {
+			foreach (ButtonComponent bc in currentPrompt.buttonComponents) {
+				if (GUILayout.Button (bc.Text)) {
+					if ((bc.TextPrompt != "") && (bc.TextPrompt != null)) {
+						responseMode = true;
+						responseText = bc.TextPrompt;
+					}
+					currentPromptID = bc.FollowUp;
+					videoID = bc.VideoFollowUpId;
+				
+				}
+			}
+		}
+	}
+}
---
+++ RockPlacementPrototype/Assets/TestScripts/MovieTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieTest : MonoBehaviour {
+
+	public Material movFile;
+	void Start()
+	{
+		movFile = Resources.Load<Material> ("Materials/SampleVideo_1080x720_2mb 1");
+	}
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+
+			MovieTexture movie = (MovieTexture)movFile.mainTexture;
+
+			if (movie.isPlaying) {
+				movie.Pause();
+			}
+			else {
+				movie.Play();
+			}
+		}
+	}
+}
